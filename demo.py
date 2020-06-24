167b446d311bafe26ae90c118b373aaffb9e47c0
Johnnatan Messias <johnme@mpi-sws.org>
2020-06-24 13:38:44 +0200

    Replaces gif

--- demo.py
+++ demo.py
@@ -11,15 +11,14 @@ from political_leaning_api import Political_Leaning
 
 
 demo = Political_Leaning(token="Your_Token_Here")
-demo.get_info()
 
 
 def main():
     inFile = gzip.open('input_test.gz')
     for line in inFile:
         data = json.loads(line)
-        print(data['user']['screen_name'])
-        print('\t', demo.get_political_leaning(friends_ids=data['friend_ids']))
+        print('username=%s' % data['user']['screen_name'])
+        print('\tresponse=%s' % demo.get_political_leaning(friends_ids=data['friend_ids']))
     inFile.close()
 
 if __name__ == "__main__":
